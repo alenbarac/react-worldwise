a1b5c130d741df2a556a2b44593ad1300bcee0e2
Alen <info@alenbarac.com>
2023-10-05 23:52:52 +0200

    ract router added to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import PageNotFound from './pages/PageNotFound'
+import Product from './pages/Product'
+
 function App() {
-  return <div>WorldWise App</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="product" element={<Product />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
