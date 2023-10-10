2d194a2ef5a134f50e768a195e0aa308b3b47522
Alen <info@alenbarac.com>
2023-10-10 10:51:47 +0200

    pages completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AppLayout from './pages/AppLayout'
-import Home from './pages/Home'
 import Homepage from './pages/Homepage'
 import PageNotFound from './pages/PageNotFound'
 import Product from './pages/Product'
+import Pricing from './pages/Pricing'
+import Login from './pages/Login'
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
