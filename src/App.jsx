4fb4a82d550d4b1b44357c845a0391c85687d96c
Alen <info@alenbarac.com>
2023-10-15 20:47:37 +0200

    redirect back with useNavigate hook

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Login from './pages/Login'
 import CityList from './components/CityList'
 import City from './components/City'
 import CountryList from './components/CountryList'
+import Form from './components/Form'
 
 const BASE_URL = 'http://localhost:8000'
 
@@ -44,7 +45,7 @@ function App() {
           <Route path="cities" element={<CityList cities={cities} loading={loading} />} />
           <Route path="cities/:id" element={<City />} />
           <Route path="countries" element={<CountryList cities={cities} loading={loading} />} />
-          <Route path="form" element={<p>Form Component</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
