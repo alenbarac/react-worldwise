5657d2692fda3def1d3e0b62f3a336e497149315
Alen <info@alenbarac.com>
2023-10-22 23:28:40 +0200

    interaction with map based on city click and map redirect to form

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { useEffect, useState } from 'react'
+import { MapContainer, Marker, Popup, TileLayer, useMap, useMapEvents } from 'react-leaflet'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import { useCities } from '../contexts/CitiesContext'
 import styles from './Map.module.css'
 
 function Map() {
   const [mapPosition, setMapPosition] = useState([40, 0])
   const { cities } = useCities()
+
+  const [searchParams] = useSearchParams()
+  const mapLat = searchParams.get('lat')
+  const mapLng = searchParams.get('lng')
+
+  useEffect(() => {
+    if (mapLat && mapLng) {
+      setMapPosition([mapLat, mapLng])
+    }
+  }, [mapLat, mapLng])
 
   const flagemojiToPNG = (flag) => {
     var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
@@ -29,9 +40,26 @@ function Map() {
             </Popup>
           </Marker>
         ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   )
+}
+
+function ChangeCenter({ position }) {
+  const map = useMap()
+  map.setView(position)
+
+  return null // component need to return some jsx - null is valid
+}
+
+function DetectClick() {
+  const navigate = useNavigate()
+
+  useMapEvents({
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+  })
 }
 
 export default Map
